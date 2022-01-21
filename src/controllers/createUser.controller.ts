6c37e514c61e80f6e7fb2e3c046dea6bce761374
Marcelo nascimento <marcelo.nascimento@skyone.solutions>
2022-01-20 21:06:19 -0300

    create new user basic

--- src/controllers/createUser.controller.ts
+++ src/controllers/createUser.controller.ts
@@ -1,18 +1,24 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { UserModel } from "../models/userModel";
 
 class CreateUser {
-  public createNewUser(request:Request, response:Response): Response {
+  async createNewUser(request:Request, response:Response): Promise<Response> {
     const { name, email, password } = request.body;
     try {
       // validar dados
       // ve se email já existe
       // criptografia da senha
-
-      return response.json({
+      const repository = getRepository(UserModel);
+      const create = repository.create({
         name,
         email,
         password,
       });
+
+      const data = await repository.save(create);
+
+      return response.status(201).json(data);
     } catch (error) {
       return response.status(500);
     }
---
+++ src/models/userModel/index.ts
@@ -0,0 +1,33 @@
+import {
+  Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("user")
+class UserModel {
+    @PrimaryGeneratedColumn("uuid")
+      id!: string;
+
+    @Column()
+      name!: string;
+
+    @Column()
+      email!: string;
+
+    @Column()
+      password!: string;
+
+    @CreateDateColumn()
+      created_at!: Date;
+
+    @UpdateDateColumn()
+      updated_at!: Date;
+
+    constructor() {
+      if (!this.id) {
+        this.id = uuid();
+      }
+    }
+}
+
+export { UserModel };
